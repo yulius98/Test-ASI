63cd13b59499d0be356b391ab8628e790574078f
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-03-25 10:51:17 +0700

    commit8

--- resources/views/my_client/create.blade.php
+++ resources/views/my_client/create.blade.php
@@ -18,18 +18,17 @@
         <label for="client_logo">Client Logo:</label>
         <input type="file" name="client_logo">
         
+        <h2>AWS Credentials</h2>
+        <label for="aws_access_key_id">AWS Access Key ID:</label>
+        <input type="text" name="aws_access_key_id" required>
+    
+        <label for="aws_secret_access_key">AWS Secret Access Key:</label>
+        <input type="text" name="aws_secret_access_key" required>
+    
+        <label for="aws_bucket">AWS Bucket Name:</label>
+        <input type="text" name="aws_bucket" required>
+    
         <button type="submit">Create Client</button>
     </form>
-    <a href="{{ route('my_client.index') }}">Back to Clients</a>
-
-    <h2>AWS Credentials</h2>
-    <label for="aws_access_key_id">AWS Access Key ID:</label>
-    <input type="text" name="aws_access_key_id" required>
-
-    <label for="aws_secret_access_key">AWS Secret Access Key:</label>
-    <input type="text" name="aws_secret_access_key" required>
-
-    <label for="aws_bucket">AWS Bucket Name:</label>
-    <input type="text" name="aws_bucket" required>
 </body>
 </html>
